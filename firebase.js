6c6a84ba2452b2c27465bff1b38a9a64e743b637
Subhrajit <subhrajit5104@gmail.com>
2026-05-01 18:15:40 +0530

    Final SmartRep project

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+  apiKey: "AIzaSyBsL6ATzAach4mkO6-EIKsFdAWUJLHHkh4",
+  authDomain: "smartrep-8ca34.firebaseapp.com",
+  projectId: "smartrep-8ca34",
+  storageBucket: "smartrep-8ca34.firebasestorage.app",
+  messagingSenderId: "874359721336",
+  appId: "1:874359721336:web:354997f4d68ab514dd8c8d",
+  measurementId: "G-Q1H88BXYGW"
+};
